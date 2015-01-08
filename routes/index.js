98d7caca7574ed75110f62b7ee6b6b10ad93e3c8
Paz23 <pasley@gmail.com>
2015-01-07 23:29:01 -0800

    put back code to prevent asking for name if client has already provided one

--- routes/index.js
+++ routes/index.js
@@ -15,7 +15,10 @@ module.exports = function Route(io, app, cookieParser){
   io.sockets.on('connection', function (socket){
     console.log('sockets are now operational');
     if(socket.handshake.headers.cookie){
-      // get cookie
+      // was just looking for socket.id - changes on refresh/reload so can't use as key in users object (to check if user has already supplied a name)
+      console.log('\n\n\n\nsocket.id', socket.id);
+
+      // get cookie to determine the connect.sid, that doesn't change on refresh/reload, to use as key in users object
       var this_cookie = cookieParser.signedCookie(socket.handshake.headers.cookie, 'codingDojo');
       // console.log('\nthis_cookie\n', this_cookie);
       // console.log('\nthis_cookie.indexof()\n', this_cookie.indexOf('connect.sid'));
@@ -26,9 +29,16 @@ module.exports = function Route(io, app, cookieParser){
       var connect_sid = this_cookie.substr(pos);
       // console.log('\nconnect_sid\n', connect_sid);
     }
-    console.log('prompt for user name', connect_sid);
-    // emit a request for user name
-    socket.emit('prompt_for_name', { id: connect_sid });
+    // logic to determine if the user is already in the users object (user has already supplied a name)
+    if(users[connect_sid]){
+      console.log('\nuser', users[connect_sid],  'already exists\n');
+      // broadcast to all users but the user that connected
+      socket.broadcast.emit('new_user', users[connect_sid]);
+    } else {
+      console.log('\nprompt for user name\n', connect_sid);
+      // emit a request for user name
+      socket.emit('prompt_for_name', { id: connect_sid });
+    }
     
     // listen for new user to emit name
     socket.on('new_user_name', function (data){
@@ -56,9 +66,7 @@ module.exports = function Route(io, app, cookieParser){
       // console.log('\nname\n', name);
       // broadcast to all other users the user that has left the chat
       socket.broadcast.emit('user_departed', name);
-      // remove user who disconnected from array
-      delete users[connect_sid];
-      // console.log('\ndisconnect data', connect_sid, '\nxxx');
+      // console.log('\ndisconnect data', connect_sid, ' ', users[connect_sid], '\nxxx');
     });
 
   }); 
